973df10104e9fb8944d112928b29a7bdca7defc4
siddhiparte24 <siddhiparte24@gmail.com>
2024-10-23 18:46:41 +0530

    changed cors url

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://your-frontend-url.vercel.app"  # Update with your Vercel URL
+        "https://nextjs-frontend-jet.vercel.app/"  # Update with your Vercel URL
     ],
     allow_credentials=True,
     allow_methods=["*"],
